c457809d1671ec4ee340aa5315996e8717094610
NirinaMickael <todisoanirinamickael@gamil.com>
2022-11-09 20:21:29 +0300

    auth rectification

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-  hide = true
-  loginForm !: FormGroup
-  constructor( ) {
-
-  }
+  hide = true;
+  loginForm!: FormGroup;
+  constructor() {}
 
   ngOnInit() {
-    this.loginForm = new FormGroup(
-      {
-        email: new FormControl('',[Validators.required,Validators.email]),
-        password: new FormControl('', [Validators.required,Validators.minLength(6)])
-      }
-    );
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
-onLogin(){
-  console.log(this.loginForm.get('email'));
-  console.log(this.loginForm.get('email'));
-}
+  onLogin() {
+    console.log(this.loginForm.invalid);
+  }
 }
