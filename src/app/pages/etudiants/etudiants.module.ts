5c0062cbe04fa92b997ca188f6264c6f52c87b71
NirinaMickael <todisoanirinamickael@gamil.com>
2022-11-11 08:58:45 +0300

    gjhjkk

--- src/app/pages/etudiants/etudiants.module.ts
+++ src/app/pages/etudiants/etudiants.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { EtudiantsRoutingModule } from './etudiants-routing.module';
 import { EtudiantsComponent } from './etudiants.component';
 import { EmpruntComponent } from 'src/app/features/etudiants/emprunt/emprunt.component';
